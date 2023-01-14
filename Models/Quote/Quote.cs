0e48074a3fcd8c18482c894dc34d7cc80ef69196
PJ McGarrity <pmcgarrity@outlook.com>
2023-01-14 15:44:52 +0000

    initial commit

---
+++ Models/Quote/Quote.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestWebsite.Models.Quote
+{
+    public class Quote
+    {
+        [Display(Name = "Contact Name")]
+        public string ContactName { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Total Size (m2)")]
+        public double TotalSize { get; set; }
+
+        [Display(Name = "Total Rebuild Cost (£)")]
+        public decimal TotalRebuildCost { get; set; }
+    }
+}
